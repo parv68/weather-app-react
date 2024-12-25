fb88deb91b44cad4f1d850dc676bd7a503251187
Parv <parvruhil68@gmail.com>
2024-12-25 21:14:29 +0530

    vite file updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/parv68/weather-app-react/'
+  base: '/weather-app-react/'
 })
